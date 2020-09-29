eefefa83af6591c212ec3ca381c3fa708b7268b0
EnriqueWiggin <70081772+EnriqueWiggin@users.noreply.github.com>
2020-09-28 21:20:39 -0400

    Add files via upload

--- Paper.js
+++ Paper.js
@@ -1,27 +1,37 @@
-class Paper {
-  constructor(x, y) {
-    var options = {
-        'restitution':0.3,
-        'friction':0.5,
-        'density':1.2
-    }
-    this.body = Bodies.circle(x, y, 12, options);
-    this.radius = 12;
-    this.height = 50;
-    
-    World.add(world, this.body);
-  }
-  display(){
-    var pos =this.body.position;
-    var radius = this.body.radius;
-    push();
-    translate(pos.x, pos.y);
-    rotate(radius);
-    circleMode(CENTER);
-    fill("white");
-    circle(0, 0, this.radius, this.height);
-    pop();
-  }
-};
+class paper
+{
+	constructor(x,y,r)
+	{
+		var options={
+			isStatic:false,
+			restitution:0.3,
+			friction:0.5,
+			density:1.2
 
+			
+			}
+		this.x=x;
+		this.y=y;
+		this.image=loadImage("paper.png")
+		this.r=r
+		this.body=Bodies.circle(this.x, this.y, this.r/2, options)
+		World.add(world, this.body);
 
+	}
+	display()
+	{
+			
+			var paperpos=this.body.position;		
+
+			push()
+			translate(paperpos.x, paperpos.y);
+			rectMode(CENTER)
+			strokeWeight(3);
+			fill(255,0,255)
+			imageMode(CENTER); 
+			image(this.image, 0,0,this.r, this.r)
+			pop()
+			
+	}
+
+}
--- sketch.js
+++ sketch.js
@@ -1,75 +1,48 @@
-
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
+const Render = Matter.Render;
+var dustbinObj, paperObject,groundObject	
+var world;
 
-var rightBody;
-var leftBody;
-var bottomBody;
-var ground;
-var paperBall;
-
-var engine,world;
-function preload()
-{
-	
-}
 
 function setup() {
-	createCanvas(800, 700);
+	createCanvas(1600, 700);
+	rectMode(CENTER);
+
+
 	engine = Engine.create();
 	world = engine.world;
-
-
-	rightSideBin=createSprite(250,610,20,100);
-	rightSideBin.shapeColor=("white");
-
-	rightBody=Bodies.rectangle(250,610,20,100, {isStatic:true})	
-	World.add(world, rightBody)
-	conmouseleave.log(rightBody);
-
-	leftSideBin=createSprite(450,610,20,100);
-	leftSideBin.shapeColor=("white");
-
-	leftBody=Bodies.rectangle(450,610,20,100, {isStatic:true})
-	World.add(worlds, leftBody)
-	conmouseleave.log(leftBody);
-
-	bottomBin=createSprite(350,650,200,20);
-	bottomBin.shapeColor=("white");
-
-	bottomBody=Bodies.rectangle(350,650,200,20, {isStatic:true})
-	World.add(worlds, bottomBody)
-	conmouseleave.log(bottomBody);
-
-	ground = Bodies.rectangle(width/2, 655, width, 10 , {isStatic:true} );
-	 World.add(world, ground);
-	 
-	 paperBall = Bodies.circle(600,650,12);
-	 World.add(world, paperBall);
-
-	
-
+	dustbinObj=new dustbin(1200,650);
+	paperObject=new paper(200,450,40);
+	groundObject=new ground(width/2,670,width,20);
+	//Create a Ground
 
 	Engine.run(engine);
+	//Render.run(render);
   
 }
 
 
 function draw() {
- 
-  background(0);
   rectMode(CENTER);
-    drawSprites();
+  background(255);
+ 
+  dustbinObj.display();
+  paperObject.display();
+  groundObject.display();
+  
+ 
+  
+  
  
 }
 
 function keyPressed() {
-	if (keyCode===UP_ARROW) {
-		Matter.body.applyForce(paperObject.body,paperObject.body.position,{x:85,y:-85});
-	 }
-	}
+  	if (keyCode === UP_ARROW) {
 
-
-
+    	Matter.Body.applyForce(paperObject.body,paperObject.body.position,{x:85,y:-85});
+    
+  	}
+}
